44e1f15baaee9ea19d5726beb2eef248e9a8ad04
bac1lla <2365434@mail.ru>
2022-06-19 02:39:11 +0300

    function read done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,31 @@
+import path from "node:path";
+import {fileURLToPath} from "url";
+import fs from "node:fs";
+
 export const read = async () => {
-    // Write your code here 
+
+    let dirName = "files"
+    let filename = "fileToRead.txt"
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    let filePath = path.join(__dirname, dirName, filename)
+
+    fs.access(filePath, err => {
+        if (err) {
+            // console.log("FS operation failed");
+            // console.log(`${filename} in ${filePath} doesn't exist`);
+            throw new Error("FS operation failed");
+        }
+    })
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            console.log("FS operation failed");
+            console.log(`Unable to read ${filename}`);
+            throw new Error("FS operation failed");
+        }
+        console.log(data)
+    })
 };
+
+read()
