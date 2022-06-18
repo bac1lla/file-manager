f39460e276fa54602d33b715ec2c93eb910b223a
bac1lla <2365434@mail.ru>
2022-06-18 23:05:49 +0300

    function create done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,23 +1,22 @@
 import * as fs from 'node:fs';
 import * as path from "node:path"
+import { fileURLToPath } from 'url';
 
 export const create = async () => {
-    let directoryPath = "./src/fs/files"
     let fileName = "fresh.txt"
     let fileContent = "I am fresh and young"
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-    let finalPath = path.join("./", directoryPath, fileName)
+    let finalPath = path.join(__dirname, "files", fileName)
 
     fs.access(finalPath, (err) => {
         if (err) {
-            fs.writeFile(`${directoryPath}/${fileName}`, fileContent, (err) => {
+            fs.writeFile(finalPath, fileContent, (err) => {
                 if (err) throw err;
-                console.log("The file was succesfully saved!");
+                console.log("The file was successfully saved!");
             });
         } else {
             console.log("FS operation failed");
         }
     });
-
-
 };
