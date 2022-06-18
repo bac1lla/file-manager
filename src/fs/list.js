c66a4a5ac5e2a375bf4ac857d61714f5d89f6a4f
bac1lla <2365434@mail.ru>
2022-06-19 02:31:11 +0300

    function list done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,26 @@
+import path from "node:path";
+import {fileURLToPath} from "url";
+import fs from "node:fs";
+
 export const list = async () => {
-    // Write your code here 
+
+    let dirName = "files"
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+    let dirPath = path.join(__dirname, dirName)
+
+    fs.access(dirPath, err => {
+        console.log("FS operation failed");
+        console.log(`Directory ${dirName} doesn't exist`);
+        return null
+    })
+
+    fs.readdir(dirPath, (err, files) => {
+        if (err) {
+            console.log("FS operation failed");
+            console.log(`Unable to read files in directory ${dirName}`);
+            return null
+        }
+        console.log(files)
+    })
 };
