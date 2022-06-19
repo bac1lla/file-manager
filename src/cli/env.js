ca54a2fa5dc7089e3ef14c55a7e58075ab8419a0
bac1lla <2365434@mail.ru>
2022-06-19 09:00:49 +0300

    function parseEnv done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
 export const parseEnv = () => {
-    // Write your code here 
+    let prefix = "USER"
+    let env = process.env
+    let parseEnvStr = `^${prefix}(.*)`
+    let parseEnvReg = new RegExp(parseEnvStr)
+
+    for (let key in env) {
+        if (key.match(parseEnvReg)) {
+            process.stdout.write(`${key}=${env[key]}; `)
+        }
+    }
 };
+
+parseEnv()
+
+
+
