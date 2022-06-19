905d66c702a22f8645fe5e309fd28f31f77a1e10
bac1lla <2365434@mail.ru>
2022-06-19 09:25:45 +0300

    function parseArgs done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
-    // Write your code here 
+    let args = process.argv
+
+    for (let i = 1; i < args.length; i++) {
+        if (args[i - 1].match(/^--.*/) && args[i].match(/^(?!--).+/)) {
+            console.log(`${args[i - 1]} is ${args[i]} `)
+        }
+    }
 };
+
+
+
+
